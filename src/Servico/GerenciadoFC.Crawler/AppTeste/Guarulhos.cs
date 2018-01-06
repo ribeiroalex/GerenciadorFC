c5157df2eb588237e723d03d942c921d6f7c3a11
Erivelto <fabioesimoes@gmail.com>
2018-01-06 20:51:39 -0200

    Select guarulhos
    Select guarulhos

---
+++ src/Servico/GerenciadoFC.Crawler/AppTeste/Guarulhos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTeste
+{
+    public class Guarulhos  
+    {
+        public static GerenciadorFC.Tomador.Tomador get_Tomador()
+        {
+            var tomador = new GerenciadorFC.Tomador.Tomador();
+            tomador.Email = "fabioesimoes@gmail.com";
+            tomador.Documento = "07.986.975/0001-80";
+            tomador.Cidade = "SAO PAULO";
+            tomador.Endereco = "RUA HADDOCK LOBO";
+            tomador.RazaoSocial = "TECH FOR PARTICIPAÇÕES & SISTEMAS EM TECNOLOGIA DA INFORMAÇÃO LTDA";
+            tomador.Fantasia = "";
+            tomador.TipoPessoa = "PJ";
+            tomador.UF = "SP";
+            tomador.Numero = "337";
+            tomador.Bairro = "CERQUEIRA CÉSAR";
+            tomador.CEP = "01414-001";
+
+            return tomador;
+        }
+        public static GerenciadorFC.Prestador.Prestador get_Prestador()
+        {
+            var prestador = new GerenciadorFC.Prestador.Prestador();
+            prestador.Valor = "100,10";
+            prestador.Usuario = "293161";
+            prestador.Senha = "293161DBN85553";
+            prestador.RazaoSocial = "ERIVELTO SOLUCOES E TECNOLOGIAS EIRELI ME";
+            prestador.Fantasia = "ERIVELTO SOLUCOES E TECNOLOGIAS";
+            prestador.Endereco = "RUA GAGO COUTINHO ,354 - JARDIM VILA GALVÃO CEP: 07055-030";
+            prestador.Documento = "27.308.027/0001-00";
+            prestador.InscricaoMunicipal = "293161";
+            prestador.UF = "SP";
+            prestador.Estado = "SAO PAULO";
+            prestador.Tipo = "PJ";
+            prestador.Cidade = "GUARULHOS";
+            prestador.CodigoServico = "1.01 / 722900001";
+            prestador.Discriminacao = "Serviço de desenvolvimento de software Valor de : R$ 100,00 ";
+            prestador.UlrLogin = "http://guarulhos.ginfes.com.br/";
+            prestador.Email = "fabioesimoes@gmail.com";
+
+            return prestador;
+        }
+    }
+}
---
+++ src/Servico/GerenciadoFC.Crawler/AppTeste/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GerenciadorFC.Prestador;
+using GerenciadorFC.Tomador;
+
+
+
+namespace AppTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prestador = new Prestador();
+            var tomador = new Tomador();
+            var guarulhosNfe = new GerenciadorFC.Robo.Guarulhos.GeradorNfe();
+            //var arujaNfe = new GerenciadorFC.Robo.Aruja.GeradorNfe();
+            //var arujaCancel = new GerenciadorFC.Robo.Aruja.CancelaNfe();
+
+            tomador = AppTeste.Guarulhos.get_Tomador();
+            prestador = AppTeste.Guarulhos.get_Prestador();
+
+            //tomador = AppTeste.Aruja.get_Tomador();
+            //prestador = AppTeste.Aruja.get_Prestador();
+
+            Task.Run(() => guarulhosNfe.Emissor(prestador, tomador)).Wait();
+            //string nfe =  arujaNfe.Emissor(prestador, tomador);
+
+            // arujaCancel.Cancelar("39", prestador);
+                       
+
+        }
+    }
+}
