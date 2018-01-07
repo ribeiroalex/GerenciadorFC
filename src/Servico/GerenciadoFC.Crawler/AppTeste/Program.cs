677e2b89ffefed70f500300a1b7c04feca730b10
Erivelto <fabioesimoes@gmail.com>
2018-01-07 06:57:05 -0200

    Concluido Prefeitura de Itaquaquecetuba
    Concluido Prefeitura de Itaquaquecetuba

--- src/Servico/GerenciadoFC.Crawler/AppTeste/Program.cs
+++ src/Servico/GerenciadoFC.Crawler/AppTeste/Program.cs
@@ -16,22 +16,29 @@ namespace AppTeste
         {
             var prestador = new Prestador();
             var tomador = new Tomador();
-            var guarulhosNfe = new GerenciadorFC.Robo.Guarulhos.GeradorNfe();
+            //var guarulhosNfe = new GerenciadorFC.Robo.Guarulhos.GeradorNfe();
             //var arujaNfe = new GerenciadorFC.Robo.Aruja.GeradorNfe();
             //var arujaCancel = new GerenciadorFC.Robo.Aruja.CancelaNfe();
-            var arujaConsulta = new GerenciadorFC.Robo.Aruja.ConsultaNFe();
-
+            //var arujaConsulta = new GerenciadorFC.Robo.Aruja.ConsultaNFe();
+            var itaquaNfe = new GerenciadorFC.Robo.Itaquaquecetuba.ConsultaNfe();
             //tomador = AppTeste.Guarulhos.get_Tomador();
             //prestador = AppTeste.Guarulhos.get_Prestador();
 
-            tomador = AppTeste.Aruja.get_Tomador();
-            prestador = AppTeste.Aruja.get_Prestador();
+            //tomador = AppTeste.Aruja.get_Tomador();
+            //prestador = AppTeste.Aruja.get_Prestador();
+
+            tomador = AppTeste.Itaquaquecetuba.get_Tomador();
+            prestador = AppTeste.Itaquaquecetuba.get_Prestador();
 
             //Task.Run(() => guarulhosNfe.Emissor(prestador, tomador)).Wait();
             //string nfe =  arujaNfe.Emissor(prestador, tomador);
 
-            arujaConsulta.Consulta("40", prestador);
-                       
+            ///arujaConsulta.Consulta("40", prestador);
+            ///
+            ///itaquaNfe.Emissor(prestador, tomador);
+            itaquaNfe.Consulta("739", prestador);
+
+
 
         }
     }
