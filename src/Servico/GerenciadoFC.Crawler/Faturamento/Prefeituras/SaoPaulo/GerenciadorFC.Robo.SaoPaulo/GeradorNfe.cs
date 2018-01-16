72081518bf8f1aa10fe365f430017d35ee5240e3
ribeiroalex <aleribeiro@outlook.com>
2018-01-16 01:00:16 -0200

    alteraçoes

--- src/Servico/GerenciadoFC.Crawler/Faturamento/Prefeituras/SaoPaulo/GerenciadorFC.Robo.SaoPaulo/GeradorNfe.cs
+++ src/Servico/GerenciadoFC.Crawler/Faturamento/Prefeituras/SaoPaulo/GerenciadorFC.Robo.SaoPaulo/GeradorNfe.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -10,25 +9,11 @@ using System.Linq;
 using System.IO;
 using Anticaptcha_example.Api;
 using Anticaptcha_example.Helper;
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Text.RegularExpressions;
-using System.Net;
-using System.IO;
-using OpenQA.Selenium.Firefox;
->>>>>>> cdbbfc28abfa3f0ef58d9d683ce9d0d18ab9d8a1
 
 namespace GerenciadorFC.Robo.SaoPaulo
 {
     public class GeradorNfe
     {
-<<<<<<< HEAD
         public async Task AcessarReceita()
         {
             IWebDriver driver = new ChromeDriver(@"C:\Users\Financeiro\Documents\Alex"); // new RemoteWebDriver(new  Uri( "http://localhost:9515"), DesiredCapabilities.Chrome());
@@ -55,7 +40,7 @@ namespace GerenciadorFC.Robo.SaoPaulo
 
                 binatyStream.Write(base64Array);
             }
-            var captchaText = ReadImage(filePath);
+            var captchaText = ReadImage(filePath, "COLOCAR_API_KEY_ESTA NO EMAIL");
 
             var captchaInputText = driver.FindElement(By.Id("txtTexto_captcha_serpro_gov_br"));
 
@@ -130,47 +115,5 @@ namespace GerenciadorFC.Robo.SaoPaulo
 
         //    return cookieContainer;
         //}
-=======
-        public string Emissor(GerenciadorFC.Prestador.Prestador prestador, GerenciadorFC.Tomador.Tomador tomador)
-        {
-            string imageSrc = "";
-            IWebDriver driver = new FirefoxDriver(@"C:\Users\fabio\.nuget\packages\Selenium.Firefox.WebDriver\0.19.1\driver");
-            driver.Navigate().GoToUrl(prestador.UlrLogin);
-
-            var cnpj = driver.FindElement(By.Id("ctl00_body_tbCpfCnpj"));
-            cnpj.SendKeys(prestador.Usuario);
-            var senha = driver.FindElement(By.Id("ctl00_body_tbSenha"));
-            senha.SendKeys(prestador.Senha);
-
-            IWebElement html = driver.FindElement(By.TagName("html"));
-            List<IWebElement> tabelaList = new List<IWebElement>(html.FindElements(By.TagName("table")));
-            for (int i = 0; i < tabelaList.Count(); i++)
-            {
-                if (i == 2)
-                {
-                    IWebElement tabela = tabelaList[i];
-                    List<IWebElement> tdList = new List<IWebElement>(tabela.FindElements(By.TagName("td")));
-                    for (int i2 = 0; i2 < tdList.Count(); i2++)
-                    {
-                        if (i2 == 6)
-                        {
-                            IWebElement td = tdList[i2];
-                            var img = td.FindElement(By.TagName("img"));
-                            imageSrc = img.GetAttribute("src");
-                        }
-                    }
-                }
-            }
-            //byte[] data;
-            //using (WebClient client = new WebClient())
-            //{
-            //    data = client.DownloadData("view-source:" + imageSrc);
-            //}
-            //File.WriteAllBytes(@"C:\Users\fabio\Downloads\images\prefeitura.jpg", data);
-
-
-            return "";
-        }
->>>>>>> cdbbfc28abfa3f0ef58d9d683ce9d0d18ab9d8a1
     }
 }
