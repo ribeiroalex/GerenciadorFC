dfaefc945a5dddaaa3ee44dff946ea211b63d46e
ribeiroalex <aleribeiro@outlook.com>
2018-02-19 23:39:36 -0300

    merge main -> desenvolvimento

---
+++ src/Servico/GerenciadoFC.Crawler/GerenciadorFC.Contribuinte/AnexoContribuinte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorFC.Contribuinte
+{
+    public class AnexoContribuinte
+    {
+        public int Codigo { get; set; }
+        public string Anexo { get; set; }
+        public int Contribuite { get; set; }
+    }
+}
